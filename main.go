2f51089c3e9be63caca6dd55cab2fe27fe72ef09
Your Name <you@example.com>
2019-07-01 18:04:57 +0700

    more api

--- main.go
+++ main.go
@@ -1,13 +1,31 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	router := gin.Default()
+	// test api
+	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	// get params
+	router.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name)
+	})
+	// post request
+	router.POST("post", func(c *gin.Context) {
+		// id := c.PostForm("id")
+		name := c.PostForm("name")
+		c.String(http.StatusOK, "Hello %s", name)
+
+	})
+
+	router.Run() // listen and serve on 0.0.0.0:8080
 }
