96f5f55d37a5ba608e9b87a669bffa8d1f72a0ce
Your Name <you@example.com>
2019-07-03 14:28:17 +0700

    delete success

--- controllers/users/delete.go
+++ controllers/users/delete.go
@@ -2,11 +2,22 @@ package users
 
 import (
 	"github.com/gin-gonic/gin"
+	"../../models"
 )
 
 func Delete(c *gin.Context) {
 	id := c.Param("id")
+	db:= models.DBConn()
+	delete, err := db.Prepare("DELETE FROM users WHERE id=" + id)
+    if err != nil {
+        c.JSON(500, gin.H{
+			"message":err,
+		})
+		return
+	}
+	delete.Exec()
 	c.JSON(200, gin.H{
-		"Delete": id,
+		"Delete": "deleted",
 	})
+	defer db.Close()
 }
