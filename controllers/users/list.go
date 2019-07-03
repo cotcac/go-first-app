5f1b3a42d052d77283f1828427c7b3c5a0c71f8b
Your Name <you@example.com>
2019-07-03 13:21:48 +0700

    list all users

--- controllers/users/list.go
+++ controllers/users/list.go
@@ -2,10 +2,33 @@ package users
 
 import (
 	"github.com/gin-gonic/gin"
+	"../../models"
 )
 
+type User struct {
+	Id int `json:"id"`
+	Name string `json:"name"`
+}
+
 func List(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "List Users",
-	})
+	db:= models.DBConn()
+	rows, err := db.Query("select * from users")
+	if err != nil {
+		c.JSON(500, gin.H {
+			"message":"server error",
+		})
+	}
+
+	users := make([]User,0)
+	for rows.Next() {
+		var user User
+		rows.Scan(&user.Id, &user.Name)
+		users = append(users,user)
+	}
+	if err = rows.Err(); err != nil {
+	    c.JSON(500, gin.H{"error:":"server error"})
+	    return
+	}
+	c.JSON(200,users)
+	defer db.Close()
 }
