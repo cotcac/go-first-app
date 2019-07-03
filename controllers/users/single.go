16455c5bc5fcd23e5e2a29283f8486c6230d483b
Your Name <you@example.com>
2019-07-03 13:55:54 +0700

    query single row

--- controllers/users/single.go
+++ controllers/users/single.go
@@ -2,11 +2,28 @@ package users
 
 import (
 	"github.com/gin-gonic/gin"
+	"../../models"
 )
 
 func Single(c *gin.Context) {
+	type User struct {
+		Id int `json:"id"`
+		Name string `json:"name"`
+	}
 	id := c.Param("id")
+	var user User
+	db:= models.DBConn()
+	err := db.QueryRow("select * from users where id =?", id).Scan(
+		&user.Id,
+		&user.Name,
+	)
+	if err != nil {
+		c.JSON(500, gin.H {
+			"message":"server error",
+		})
+		return
+	}
 	c.JSON(200, gin.H{
-		"message": id,
+		"result": user,
 	})
 }
